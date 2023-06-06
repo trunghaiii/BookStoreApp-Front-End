4225b8a3d616fe9dea88e0f09a0780e9aa50496f
trunghaiii <haitran022001@gmail.com>
2023-06-05 22:21:19 -0400

    set up redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-
+import type { RootState } from './redux/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/counterSlice'
 
 function App() {
 
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
     <>
-      Hello World With BookStoreApp Hehedd
+      <div>BookStore React Ts + Redux</div>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrementtt
+          </button>
+        </div>
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { store } from './redux/store.tsx'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
