9c8cb3293b15ea61e20bb42f67ad4b0ade00d9e7
trunghaiii <haitran022001@gmail.com>
2023-06-06 20:36:31 -0400

    Design Login Page with Ant Design

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,68 @@
+import React from 'react';
+import './Login.scss';
+import { useNavigate } from "react-router-dom";
+import { Button, Checkbox, Form, Input, Divider } from 'antd';
 
+const onFinish = (values: any) => {
+    console.log('Success:', values);
+};
 
-const Login = () => {
+// const onFinishFailed = (errorInfo: any) => {
+//     console.log('Failed:', errorInfo);
+// };
+
+const Login: React.FC = () => {
+    const navigate = useNavigate();
 
     return (
-        <div>
-            Login
+        <div className='login-page'>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600, margin: '0 auto' }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                // onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <h1 style={{ textAlign: 'center', marginTop: 100 }}>Login</h1>
+                <Divider />
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your Email!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Login
+                    </Button>
+                </Form.Item>
+                <span className='or-text'>Or</span>
+                <p>You do not have account yet?
+                    <span
+                        className='register-btn'
+                        onClick={() => navigate("/register")}
+                    >Register</span></p>
+                <Divider />
+            </Form>
         </div>
     )
-}
-
+};
 
 export default Login;
