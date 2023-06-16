bf0aa508f57ee2afeae67372ff86b27d62251b2b
trunghaiii <haitran022001@gmail.com>
2023-06-15 23:45:04 -0400

    fix bug refresh page when access token expired with retry in axios

--- src/utils/axiosCustomize.ts
+++ src/utils/axiosCustomize.ts
@@ -7,6 +7,15 @@ const instance = axios.create({
 
 instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem("access_token")}` }
 
+const handleRefreshToken = async () => {
+    let response = await instance.get("api/v1/auth/refresh")
+    if (response && response.data) {
+        return response.data.access_token
+    }
+
+    return null
+
+}
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
@@ -21,10 +30,17 @@ instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response;;
-}, function (error) {
+}, async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    console.log("error", error);
+    if (error.config && error.response && +error.response.status === 401) {
+        let newAccessToken = await handleRefreshToken()
+        if (newAccessToken) {
+            localStorage.setItem("access_token", newAccessToken)
+            error.config.headers['Authorization'] = `Bearer ${newAccessToken}`
+            return instance.request(error.config);
+        }
+    }
 
     return error && error.response && error.response.data ?
         error.response.data : Promise.reject(error);
