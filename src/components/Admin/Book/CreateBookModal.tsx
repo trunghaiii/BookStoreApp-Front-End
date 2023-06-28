ea422e86de74318ac85e52517218d8a77ff3a159
trunghaiii <haitran022001@gmail.com>
2023-06-27 22:34:04 -0400

    style create book modal using scss

--- src/components/Admin/Book/CreateBookModal.tsx
+++ src/components/Admin/Book/CreateBookModal.tsx
@@ -1,8 +1,8 @@
 
 import React, { useState } from 'react';
-// import './index.css';
+import './CreateBookModal.scss';
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Modal, Form, Input, InputNumber, Select, Upload } from 'antd';
+import { Modal, Form, Input, InputNumber, Select, Upload } from 'antd';
 
 interface Iprops {
     showCreateBookModal: boolean;
@@ -28,128 +28,148 @@ const CreateBookModal = (props: Iprops) => {
     };
 
     return (
-        <Modal title="Create a New Book" open={showCreateBookModal} onOk={handleOk} onCancel={handleCancel}>
-            <Form
-                form={form}
-                name="basic"
-                labelCol={{ span: 24 }}
-                wrapperCol={{ span: 24 }}
-                style={{ maxWidth: 600 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Book Name"
-                    name="name"
-                    rules={[{ required: true, message: 'Please input Book Name!' }]}
+        <div className='create-book-modal-container'>
+            <Modal
+                width="50%"
+                className='create-book-modal'
+                title="Create a New Book"
+                open={showCreateBookModal}
+                onOk={handleOk}
+                onCancel={handleCancel}>
+                <Form
+                    form={form}
+                    name="basic"
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
+                    // style={{ maxWidth: 900 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    autoComplete="off"
                 >
-                    <Input />
-                </Form.Item>
+                    <div className='text-input-group'>
+                        <Form.Item
+                            className='text-input'
+                            label="Book Name"
+                            name="name"
+                            rules={[{ required: true, message: 'Please input Book Name!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                <Form.Item
-                    label="Author"
-                    name="author"
-                    rules={[{ required: true, message: 'Please input Author!' }]}
-                >
-                    <Input />
-                </Form.Item>
+                        <Form.Item
+                            className='text-input'
+                            label="Author"
+                            name="author"
+                            rules={[{ required: true, message: 'Please input Author!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                    </div>
 
-                <Form.Item
-                    label="Price"
-                    name="price"
-                    rules={[{ required: true, message: 'Please input Price!' }]}
-                >
-                    <InputNumber addonAfter="$" min={1} max={100000} />
-                </Form.Item>
+                    <div className='number-input-group'>
+                        <Form.Item
+                            className='number-input'
+                            label="Price"
+                            name="price"
+                            rules={[{ required: true, message: 'Please input Price!' }]}
+                        >
+                            <InputNumber addonAfter="$" min={1} max={100000} />
+                        </Form.Item>
 
-                <Form.Item
-                    label="Genre"
-                    name="genre"
-                    rules={[{ required: true, message: 'Please input Genre!' }]}
-                >
-                    <Select
-                        showSearch
-                        placeholder="Select a person"
-                        optionFilterProp="children"
-                        filterOption={(input, option) =>
-                            (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-                        }
-                        options={[
-                            {
-                                value: 'Arts',
-                                label: 'Arts',
-                            },
-                            {
-                                value: 'Business',
-                                label: 'Business',
-                            },
-                            {
-                                value: 'Teen',
-                                label: 'Teen',
-                            },
-                            {
-                                value: 'Cooking',
-                                label: 'Cooking',
-                            },
-                            {
-                                value: 'Entertainment',
-                                label: 'Entertainment',
-                            },
-                            {
-                                value: 'History',
-                                label: 'History',
-                            },
-                            {
-                                value: 'Music',
-                                label: 'Music',
-                            },
-                            {
-                                value: 'Sports',
-                                label: 'Sports',
-                            },
-                            {
-                                value: 'Travelling',
-                                label: 'Travelling',
-                            },
-                        ]}
-                    />
-                </Form.Item>
-                <Form.Item
-                    label="Quantity"
-                    name="quantity"
-                    rules={[{ required: true, message: 'Please input Quantity!' }]}
-                >
-                    <InputNumber min={1} max={100000} />
-                </Form.Item>
-                <Form.Item
-                    label="Sold"
-                    name="sold"
-                    rules={[{ required: true, message: 'Please input Sold number!' }]}
-                >
-                    <InputNumber min={1} max={100000} />
-                </Form.Item>
+                        <Form.Item
+                            className='number-input'
+                            label="Genre"
+                            name="genre"
+                            rules={[{ required: true, message: 'Please input Genre!' }]}
+                        >
+                            <Select
+                                showSearch
+                                placeholder="Select a genre"
+                                optionFilterProp="children"
+                                filterOption={(input, option) =>
+                                    (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                                }
+                                options={[
+                                    {
+                                        value: 'Arts',
+                                        label: 'Arts',
+                                    },
+                                    {
+                                        value: 'Business',
+                                        label: 'Business',
+                                    },
+                                    {
+                                        value: 'Teen',
+                                        label: 'Teen',
+                                    },
+                                    {
+                                        value: 'Cooking',
+                                        label: 'Cooking',
+                                    },
+                                    {
+                                        value: 'Entertainment',
+                                        label: 'Entertainment',
+                                    },
+                                    {
+                                        value: 'History',
+                                        label: 'History',
+                                    },
+                                    {
+                                        value: 'Music',
+                                        label: 'Music',
+                                    },
+                                    {
+                                        value: 'Sports',
+                                        label: 'Sports',
+                                    },
+                                    {
+                                        value: 'Travelling',
+                                        label: 'Travelling',
+                                    },
+                                ]}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            className='number-input'
+                            label="Quantity"
+                            name="quantity"
+                            rules={[{ required: true, message: 'Please input Quantity!' }]}
+                        >
+                            <InputNumber style={{ width: "100%" }} min={1} max={100000} />
+                        </Form.Item>
+                        <Form.Item
+                            className='number-input'
+                            label="Sold"
+                            name="sold"
+                            rules={[{ required: true, message: 'Please input Sold number!' }]}
+                        >
+                            <InputNumber style={{ width: "100%" }} min={1} max={100000} />
+                        </Form.Item>
+                    </div>
 
-                <Form.Item label="ThumbNail Images" name="bookImageFile">
-                    <Upload maxCount={1} listType="picture-card">
-                        <div>
-                            <PlusOutlined />
-                            <div style={{ marginTop: 8 }}>Upload</div>
-                        </div>
-                    </Upload>
-                </Form.Item>
+                    <div className='upload-input-group'>
+                        <Form.Item label="ThumbNail Images" name="bookImageFile">
+                            <Upload maxCount={1} listType="picture-card">
+                                <div>
+                                    <PlusOutlined />
+                                    <div style={{ marginTop: 8 }}>Upload</div>
+                                </div>
+                            </Upload>
+                        </Form.Item>
 
-                <Form.Item label="Slider Images" name="bookImageFileList">
-                    <Upload listType="picture-card">
-                        <div>
-                            <PlusOutlined />
-                            <div style={{ marginTop: 8 }}>Upload</div>
-                        </div>
-                    </Upload>
-                </Form.Item>
+                        <Form.Item label="Slider Images" name="bookImageFileList">
+                            <Upload listType="picture-card">
+                                <div>
+                                    <PlusOutlined />
+                                    <div style={{ marginTop: 8 }}>Upload</div>
+                                </div>
+                            </Upload>
+                        </Form.Item>
+                    </div>
 
-            </Form>
-        </Modal>
+                </Form>
+            </Modal>
+        </div>
     )
 }
 
