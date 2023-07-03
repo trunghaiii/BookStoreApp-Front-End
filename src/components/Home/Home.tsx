0c6bb86face5829bc4a00a74b48f51f3988fa81b
trunghaiii <haitran022001@gmail.com>
2023-07-03 18:16:01 -0400

    design HomePage (right side) using antd and scss

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,8 +3,9 @@ import './Home.scss';
 
 import { AiOutlineReload } from 'react-icons/Ai';
 
-import { Col, Row, Checkbox, Divider, InputNumber, Button } from 'antd';
+import { Col, Row, Checkbox, Divider, InputNumber, Button, Tabs, Card, Pagination } from 'antd';
 import type { CheckboxValueType } from 'antd/es/checkbox/Group';
+import type { TabsProps } from 'antd';
 
 const Home = () => {
 
@@ -13,6 +14,29 @@ const Home = () => {
     const onChange = (checkedValues: CheckboxValueType[]) => {
         console.log('checked = ', checkedValues);
     };
+
+    const items: TabsProps['items'] = [
+        {
+            key: '1',
+            label: `Popular`,
+            children: <></>,
+        },
+        {
+            key: '2',
+            label: `New`,
+            children: <></>,
+        },
+        {
+            key: '3',
+            label: `Cheap to Expensive`,
+            children: <></>,
+        },
+        {
+            key: '4',
+            label: `Expensive to Cheap`,
+            children: <></>,
+        },
+    ];
     return (
         <div className="homepage-container">
             <Row style={{ height: "100%" }} gutter={[20, 20]}>
@@ -45,7 +69,58 @@ const Home = () => {
                 </Col>
                 <Col md={20} sm={24} xs={24}>
                     <div className="homepage-rightside">
-                        homepage-rightside
+                        <div className='filter'>
+                            <Tabs defaultActiveKey="1" items={items} />
+                        </div>
+                        <div className='book-list'>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174013/BookStoreApp/lyux9lmifowtlsfupxip.jpg" alt="book-image" />
+                                </div>
+                                <div>Sorry! I am just a hooker</div>
+                                <div>200 $US</div>
+                            </Card>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174012/BookStoreApp/fblvj0lzlv3dipgzb3hu.png" alt="book-image" />
+                                </div>
+                                <div>Ronaldo And Messi</div>
+                                <div>100 $US</div>
+                            </Card>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174013/BookStoreApp/lyux9lmifowtlsfupxip.jpg" alt="book-image" />
+                                </div>
+                                <div>Sorry! I am just a hooker</div>
+                                <div>200 $US</div>
+                            </Card>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174012/BookStoreApp/fblvj0lzlv3dipgzb3hu.png" alt="book-image" />
+                                </div>
+                                <div>Ronaldo And Messi</div>
+                                <div>100 $US</div>
+                            </Card>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174013/BookStoreApp/lyux9lmifowtlsfupxip.jpg" alt="book-image" />
+                                </div>
+                                <div>Sorry! I am just a hooker</div>
+                                <div>200 $US</div>
+                            </Card>
+                            <Card style={{ width: 200, height: 250 }}>
+                                <div>
+                                    <img style={{ width: "100%", height: "150px" }} src="https://res.cloudinary.com/dbljsakpb/image/upload/v1688174012/BookStoreApp/fblvj0lzlv3dipgzb3hu.png" alt="book-image" />
+                                </div>
+                                <div>Ronaldo And Messi</div>
+                                <div>100 $US</div>
+                            </Card>
+
+                        </div>
+                        <div className='pagination'>
+                            <Pagination defaultCurrent={1} total={50} />;
+                        </div>
+
                     </div>
                 </Col>
 
