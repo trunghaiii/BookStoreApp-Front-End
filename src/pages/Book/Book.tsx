6ba0f6a9db63523633b400c4ed5524799e8710bd
trunghaiii <haitran022001@gmail.com>
2023-07-05 23:21:51 -0400

    design base layout for Book component page to view book detail

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -1,13 +1,23 @@
 
 import { useParams } from 'react-router-dom';
-
+import "./Book.scss"
 const Book = () => {
 
     let { bookId } = useParams();
     console.log("bookId", bookId);
 
     return (
-        <div>Book</div>
+        <div className='book-detail-wrapper'>
+            <div className='book-detail-container'>
+                <div className='image-group'>
+                    image group
+                </div>
+                <div className='content'>
+                    content
+                </div>
+            </div>
+        </div>
+
     )
 }
 
