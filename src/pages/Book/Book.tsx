658e8756314b3d0df685bbba3c6c6e8b34cfb56e
trunghaiii <haitran022001@gmail.com>
2023-07-06 00:17:20 -0400

    design leftside of Book componenr

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -1,16 +1,40 @@
 
 import { useParams } from 'react-router-dom';
 import "./Book.scss"
+
+import "react-image-gallery/styles/scss/image-gallery.scss";
+import ImageGallery from "react-image-gallery";
+
 const Book = () => {
 
     let { bookId } = useParams();
     console.log("bookId", bookId);
 
+    const images = [
+        {
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+    ];
+
     return (
         <div className='book-detail-wrapper'>
             <div className='book-detail-container'>
                 <div className='image-group'>
-                    image group
+                    <ImageGallery
+                        items={images}
+                        autoPlay={true}
+                        showPlayButton={false}
+
+                    />
                 </div>
                 <div className='content'>
                     content
