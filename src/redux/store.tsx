d6eae1f3d52932e758d800d426eee64727611a9e
trunghaiii <haitran022001@gmail.com>
2023-07-10 20:07:51 -0400

    set up and config redux persist to save redux data after refreshing

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/counterSlice'
 import accountReducer from './slices/accountSlice'
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    blacklist: ['account']
+}
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    account: accountReducer
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        account: accountReducer
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+export let persistor = persistStore(store)
+
+// export const store = configureStore({
+//     reducer: {
+//         counter: counterReducer,
+//         account: accountReducer
+//     },
+// })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
