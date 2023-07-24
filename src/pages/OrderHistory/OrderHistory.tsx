afe8c0fa8f88668d9285f0501ac9ee05b8c25431
trunghaiii <haitran022001@gmail.com>
2023-07-24 13:22:29 -0400

    re design OrderHistory component page

--- src/pages/OrderHistory/OrderHistory.tsx
+++ src/pages/OrderHistory/OrderHistory.tsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './OrderHistory.scss';
-import { Table } from 'antd';
+import { Button, Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { getOrderHistory } from '../../services/api';
-import ReactJson from 'react-json-view'
 import moment from "moment"
 
 interface DataType {
     key: number;
-    price: number;
     time: any;
     detail: any;
 }
 
 const columns: ColumnsType<DataType> = [
     {
-        title: 'Number',
+        title: 'Order Number',
         dataIndex: 'key',
-    },
-    {
-        title: 'Total Price ($)',
-        dataIndex: 'price',
     },
     {
         title: 'Time',
@@ -44,16 +38,8 @@ const OrderHistory = () => {
             response.data.map((order: any, index: number) => {
                 orderData.push({
                     key: index + 1,
-                    price: order.totalPrice,
                     time: moment(order.createdAt).format('DD-MM-YYYY HH:mm:ss'),
-                    detail: <ReactJson
-                        src={order.detail}
-                        name="Orders Detail"
-                        displayObjectSize={false}
-                        displayDataTypes={false}
-                        enableClipboard={false}
-                        collapsed={true}
-                    />,
+                    detail: <Button>View Detail</Button>
                 })
             })
         }
