822a585e6ea65bda364ad69487ff8743e383ff91
trunghaiii <haitran022001@gmail.com>
2023-07-30 13:27:45 -0400

    style for the footer stay at the bottom of page

--- src/components/Footer/Footerr.tsx
+++ src/components/Footer/Footerr.tsx
@@ -5,7 +5,14 @@ const { Footer } = Layout;
 const Footerr = () => {
     return (
         <div>
-            <Footer style={{ textAlign: 'center' }}>BookStore ©2023 Created by Hai Tran</Footer>
+            <Footer style={{
+                textAlign: 'center',
+                position: 'fixed',
+                left: '0',
+                bottom: '0',
+                width: '100%'
+
+            }}>BookStore ©2023 Created by Haii Tran</Footer>
         </div>
     )
 }
